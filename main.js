4d4b0d51ddf2dbbfb8f6725c6effee5b691db3ec
Nikhilmc21 <84916563+Nikhilmc21@users.noreply.github.com>
2022-09-05 10:25:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function my_keydown(e){
     console.log(keyPressed);
     if(keyPressed == '38')
     {
-        rover_image="image-removebg-preview (1).png"
+        rover_image="image-removebg-preview (3).png"
         rover_imgTag.onload = uploadrover;
         rover_imgTag.src = rover_image;
         uploadBackground();
@@ -39,7 +39,7 @@ function my_keydown(e){
     }
     if(keyPressed == '40')
     {
-        rover_image="image-removebg-preview (3).png"
+        rover_image="image-removebg-preview (1).png"
         rover_imgTag.onload = uploadrover;
         rover_imgTag.src = rover_image;
         uploadBackground();
